560a88c864723349a632d086ee9346c8578768e8
Wang-Classwork <romeowang03@gmail.com>
2020-11-25 10:19:59 +0000

    Create main.py

---
+++ Tile Game/main.py
@@ -0,0 +1,140 @@
+import pygame
+import random
+
+# -- Color Constants -- #
+
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 50, 255)
+YELLOW = (255, 255, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+
+# -- Initialise the screen -- #
+
+pygame.init()
+pygame.font.init()
+
+width = 1000
+height = 1000
+screen = pygame.display.set_mode((width, height))
+
+pygame.display.set_caption("Tile Game")
+
+class Game(pygame.sprite.Sprite):
+    def __init__(self,levels):
+
+class Player(Game):
+    def __init__(self, x, y, color, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(color)
+        # Set the position of the player
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.speed = 0
+
+    def player_set_speed(self, x, y):
+        self.rect.x += x
+        self.rect.y += y
+
+    def update(self):
+        pass
+
+
+class OuterWall(Game):
+    def __init__(self, x, y, width, height):
+        super().__init__()
+        self.image = pygame.Surface([width, height])
+        self.image.fill(GREEN)
+        # Set the position of the player
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+    def update(self):
+class InnerWall(OuterWall):
+    def __init__(self,durability):
+        super().__init__()
+map1 = [
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+]
+
+player = Player(100, 100, BLUE, 50, 50)
+
+all_sprites_group = pygame.sprite.Group()
+all_sprites_group.add(player)
+
+wall_group = pygame.sprite.Group()
+
+x = 0
+y = 0
+
+for row in map1:
+    for col in row:
+        if col == 1:
+            my_wall = OuterWall(x, y, 40, 40)
+            wall_group.add(my_wall)
+            all_sprites_group.add(my_wall)
+        x += 40
+    x = 0
+    y += 40
+
+done = False  # - Control variable
+
+clock = pygame.time.Clock()  # - Creates a pygame clock to regulate fps in game
+
+while not done:
+    # -- User input and controls
+    keys = pygame.key.get_pressed()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+
+    if event.type == pygame.KEYDOWN:
+        if keys[pygame.K_LEFT]:
+            player.player_set_speed(-1, 0)
+        if keys[pygame.K_RIGHT]:
+            player.player_set_speed(1, 0)
+        if keys[pygame.K_UP]:
+            player.player_set_speed(0, -1)
+        if keys[pygame.K_DOWN]:
+            player.player_set_speed(0, 1)
+    elif event.type == pygame.KEYUP:
+        player.player_set_speed(0, 0)
+
+        # -- Game logic goes after this comment
+
+    # -- Flip display to reveal new position of objects
+    screen.fill(BLACK)
+    all_sprites_group.draw(screen)
+    pygame.display.flip()
+    # - The clock ticks over
+    clock.tick(500)
+# End While - End of game loop
+pygame.quit()
