9be714a4fd6cca1f659156b24cc140d6128bd96a
Wang-Classwork <70944902+NexyS03@users.noreply.github.com>
2020-12-01 09:56:56 +0000

    Update main.py

--- Tile Game/main.py
+++ Tile Game/main.py
@@ -1,7 +1,9 @@
 import pygame
 import random
+import copy
+import math
 
-# -- Color Constants -- #
+# Color constants #
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -9,72 +11,19 @@ BLUE = (50, 50, 255)
 YELLOW = (255, 255, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
-
-# -- Initialise the screen -- #
-
-pygame.init()
-pygame.font.init()
-
-width = 1000
-height = 1000
-screen = pygame.display.set_mode((width, height))
-
-pygame.display.set_caption("Tile Game")
-'''
-class Game(pygame.sprite.Sprite):
-    def __init__(self):
-'''
-class Player(pygame.sprite.Sprite):
-    def __init__(self, x, y, color, width, height):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
-        # Set the position of the player
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speed = 0
-
-    def player_set_speed(self, x, y):
-        self.rect.x += x
-        self.rect.y += y
-
-    def update(self):
-        pass
-
-
-class OuterWall(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
-        super().__init__()
-        self.image = pygame.Surface([width, height])
-        self.image.fill(GREEN)
-        # Set the position of the player
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-'''
-    def update(self):
-
-class Level(Game):
-    def __init__(self,level):
-'''
-'''
-class InnerWall(OuterWall):
-    def __init__(self,durability):
-        super().__init__()
-'''
+# Maps #
 map1 = [
     (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 't', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
-    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'p', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
@@ -90,58 +39,246 @@ map1 = [
     (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
     (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
 ]
+map2 = [
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'p', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 't', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+]
+map3 = [
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 't', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'p', 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1),
+    (1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
+]
 
-player = Player(100, 100, BLUE, 50, 50)
+# Adding Maps to a list #
+AllMaps = list()
+AllMaps.append(map1)
+AllMaps.append(map2)
+AllMaps.append(map3)
 
-all_sprites_group = pygame.sprite.Group()
-all_sprites_group.add(player)
+# Initialise pygame #
 
-wall_group = pygame.sprite.Group()
+pygame.init()
+pygame.font.init()
 
-x = 0
-y = 0
 
-for row in map1:
-    for col in row:
-        if col == 1:
-            my_wall = OuterWall(x, y, 40, 40)
-            wall_group.add(my_wall)
-            all_sprites_group.add(my_wall)
-        x += 40
-    x = 0
-    y += 40
+class Level():
+    def __init__(self, level_list):
+        self.level_list = level_list
 
-done = False  # - Control variable
+    def NextLevel(self, map):
 
-clock = pygame.time.Clock()  # - Creates a pygame clock to regulate fps in game
+        width = 1000
+        height = 1000
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Tile Game")
+        wall_group = pygame.sprite.Group()
+        all_sprites_group = pygame.sprite.Group()
+        teleporter_group = pygame.sprite.Group()
+        player_group = pygame.sprite.Group()
+        bullet_group = pygame.sprite.Group()
+        innerwall_group = pygame.sprite.Group()
+        RETURN_CODE = 0
 
-while not done:
-    # -- User input and controls
-    keys = pygame.key.get_pressed()
+        class OuterWall(pygame.sprite.Sprite):
+            def __init__(self, width, height, x, y, color):
+                super().__init__()
+                self.image = pygame.Surface([width, height])
+                self.image.fill(color)
+                # Set the position of the player
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        class Player(pygame.sprite.Sprite):
+            def __init__(self, width, height, x, y, color):
+                super().__init__()
+                self.image = pygame.Surface([width, height])
+                self.image.fill(color)
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
+                self.speed_x = 0
+                self.speed_y = 0
 
-    if event.type == pygame.KEYDOWN:
-        if keys[pygame.K_LEFT]:
-            player.player_set_speed(-1, 0)
-        if keys[pygame.K_RIGHT]:
-            player.player_set_speed(1, 0)
-        if keys[pygame.K_UP]:
-            player.player_set_speed(0, -1)
-        if keys[pygame.K_DOWN]:
-            player.player_set_speed(0, 1)
-    elif event.type == pygame.KEYUP:
-        player.player_set_speed(0, 0)
+            def player_set_speed(self, x, y):
+                self.rect.x += x
+                self.rect.y += y
 
-        # -- Game logic goes after this comment
+            def shoot(self):
+                bullet = Bullet(GREEN, 4, 8, self.rect.x + 8, self.rect.y)
+                bullet_group.add(bullet)
+                all_sprites_group.add(bullet)
 
-    # -- Flip display to reveal new position of objects
-    screen.fill(BLACK)
-    all_sprites_group.draw(screen)
-    pygame.display.flip()
-    # - The clock ticks over
-    clock.tick(500)
+            def update(self):
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_a]:
+                    self.speed_x = -5
+                    self.rect.x += self.speed_x
+                if keys[pygame.K_d]:
+                    self.speed_x = 5
+                    self.rect.x += self.speed_x
+                collidecheck = pygame.sprite.spritecollide(self, wall_group, False)
+                if collidecheck:
+                    for wall in collidecheck:
+                        if self.speed_x < 0:
+                            self.rect.left = wall.rect.right
+                        if self.speed_x > 0:
+                            self.rect.right = wall.rect.left
+                if keys[pygame.K_w]:
+                    self.speed_y = -5
+                    self.rect.y += self.speed_y
+                if keys[pygame.K_s]:
+                    self.speed_y = +5
+                    self.rect.y += self.speed_y
+                collidecheck = pygame.sprite.spritecollide(self, wall_group, False)
+                if collidecheck:
+                    for wall in collidecheck:
+                        if self.speed_y < 0:
+                            self.rect.top = wall.rect.bottom
+                        if self.speed_y > 0:
+                            self.rect.bottom = wall.rect.top
+
+        class InnerWall(OuterWall):
+            def __init__(self, durability):
+                super().__init__()
+                self.durability = durability
+
+        class Bullet(pygame.sprite.Sprite):
+            def __init__(self, color, width, height, x, y):
+                super().__init__()
+                self.image = pygame.Surface([width, height])
+                self.image.fill(color)
+                self.rect = self.image.get_rect()
+                self.x = x
+                self.y = y
+                self.rect.x = self.x
+                self.rect.y = self.y
+                self.speed = 4
+
+            def update(self):
+                length = 50
+                my_player_center_x = my_player.rect.x + 10
+                my_player_center_y = my_player.rect.y + 10
+                start = [my_player_center_x, my_player_center_y]
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                end = [mouse_x,mouse_y]
+                laser = pygame.draw.line(screen, RED, start, end, 1)
+                '''collidecheck = pygame.sprite.spritecollide(laser, wall_group, False)
+                if collidecheck:
+                    for wall in collidecheck:
+                        laser.end = (my_wall.rect.x, my_wall.rect.y)'''
+
+        class Teleporter(pygame.sprite.Sprite):
+            def __init__(self, width, height, x, y, color):
+                super().__init__()
+                self.image = pygame.Surface([width, height])
+                self.image.fill(color)
+                self.rect = self.image.get_rect()
+                self.rect.x = x
+                self.rect.y = y
+
+        x = 0
+        y = 0
+        for row in map:
+            for col in row:
+                if col == 1:
+                    my_wall = OuterWall(40, 40, x, y, WHITE)
+                    wall_group.add(my_wall)
+                    all_sprites_group.add(my_wall)
+                elif col == 'p':
+                    my_player = Player(20, 20, x, y, GREEN)
+                    all_sprites_group.add(my_player)
+                    player_group.add(my_player)
+                elif col == 't':
+                    my_teleporter = Teleporter(40, 40, x, y, BLUE)
+                    teleporter_group.add(my_teleporter)
+                    all_sprites_group.add(my_teleporter)
+
+                x += 40
+            x = 0
+            y += 40
+
+        # Main game loop #
+        run = True
+        clock = pygame.time.Clock()
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    my_player.shoot()
+                if event.type == pygame.QUIT:
+                    run = False
+
+            collidecheck = pygame.sprite.spritecollide(my_player, teleporter_group, False)
+            if collidecheck:
+                RETURN_CODE = 1
+                run = False
+
+            screen.fill(BLACK)
+            all_sprites_group.draw(screen)
+            all_sprites_group.update()
+            pygame.display.flip()
+            # - The clock
+            # ticks over
+            clock.tick(60)
+
+        if RETURN_CODE == 1:
+            if len(self.level_list) != 0:
+                self.NextLevel(self.level_list.pop())
+        elif RETURN_CODE == 0:
+            pygame.quit()
+
+    def start(self):
+        self.NextLevel(self.level_list.pop())
+
+
+map_copy_list = copy.deepcopy(AllMaps)
+map_copy_list.reverse()
+new_level = Level(map_copy_list)
+new_level.start()
+
 # End While - End of game loop
-pygame.quit()
